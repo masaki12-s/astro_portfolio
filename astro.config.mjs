770839d8bf76d338ceb8563ce33deb97c95b2a8d
masaki12-s <asam.archer1112@yahoo.ne.jp>
2022-09-19 03:38:14 +0900

    baseオプション追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [preact()],
-	site: "https://masaki12-s.github.io/"
+	site: "https://masaki12-s.github.io/",
+	base: "https://github.com/masaki12-s/portfolio_astro"
 });
